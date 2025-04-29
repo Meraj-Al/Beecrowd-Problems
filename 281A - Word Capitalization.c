25d96b425c4479e7566f7813926d114819bb4cb5
Meraj-Al <almaraj26@gmail.com>
2025-04-29 10:26:11 +0600

    Add files via upload

---
+++ 281A - Word Capitalization.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>  // For toupper()
+
+int main() {
+    char word[104]; // Maximum length is 103, plus 1 for null terminator
+
+    // Read the word
+    scanf("%s", word);
+
+    // Capitalize the first letter if it's lowercase
+    if (word[0] >= 'a' && word[0] <= 'z') {
+        word[0] = toupper(word[0]);
+    }
+
+    // Print the result
+    printf("%s\n", word);
+
+    return 0;
+}
